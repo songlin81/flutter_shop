5dd0bde65224b1ea615ff9fc31a6ef1a28987b88
Lin Song <song.lin@volvo.com>
2020-06-13 18:33:13 +0800

    Migration API to AliCloud done

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,9 +1,3 @@
-//const serviceUrl = 'http://zjg5l.mocklab.io/';
-//
-//const servicePath = {
-//  'homePageContent': serviceUrl+'wxmini/homePageContent',
-//};
-
 const serviceUrl = 'http://47.104.244.246:5000/';
 
 const servicePath = {
