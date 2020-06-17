bdb6b2e20451dd6d13f6a6899fb41e8d30696732
Lin Song <song.lin@volvo.com>
2020-06-17 12:41:38 +0800

    provide added for category

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -4,6 +4,9 @@ import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provide/provide.dart';
+import '../provide/child_category.dart';
+import '../model/category.dart';
 
 class CategoryPage extends StatefulWidget {
   _CategoryPageState createState() => _CategoryPageState();
@@ -23,7 +26,12 @@ class _CategoryPageState extends State<CategoryPage>{
       body: Container(
         child: Row(
           children: <Widget>[
-            LeftCategoryNav()
+            LeftCategoryNav(),
+            Column(
+             children: <Widget>[
+               RightCategoryNav()
+             ],
+            )
           ],
         ),
       ),
@@ -38,6 +46,7 @@ class LeftCategoryNav extends StatefulWidget{
 class _LeftCategoryNavState extends State<LeftCategoryNav>{
 
   List list = [];
+  var listIndex = 0;
 
   @override
   void initState(){
@@ -64,13 +73,21 @@ class _LeftCategoryNavState extends State<LeftCategoryNav>{
   }
 
   Widget _leftInkWell(int index){
+    bool isClick = false;
+    isClick=(index==listIndex)?true:false;
     return InkWell(
-      onTap: (){},
+      onTap: (){
+        setState(() {
+          listIndex=index;
+        });
+        var childList = list[index].bxMallSubDto;
+        Provide.value<ChildCategory>(context).getChildCategory(childList);
+      },
       child: Container(
         height: ScreenUtil().setHeight(100),
         padding: EdgeInsets.only(left: 10, top: 20),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: isClick?Color.fromRGBO(236, 236, 236, 1.0):Colors.white,
           border: Border(
             bottom: BorderSide(width: 1, color: Colors.black12)
           )
@@ -87,9 +104,61 @@ class _LeftCategoryNavState extends State<LeftCategoryNav>{
       setState(() {
         list=category.data;
       });
+      Provide.value<ChildCategory>(context).getChildCategory(list[0].bxMallSubDto);
+
       //      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
 //      list.data.forEach((item) => print(item.mallCategoryName));
     });
   }
 }
+
+class RightCategoryNav extends StatefulWidget{
+  @override
+  _RightCategoryNavState createState() => _RightCategoryNavState();
+}
+
+class _RightCategoryNavState extends State<RightCategoryNav>{
+
+  //List list =['AA','BB','CC','DD','EE','FF'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Provide<ChildCategory>(
+      builder: (context, child, childCategory){
+        return Container(
+          height: ScreenUtil().setHeight(70),
+          width: ScreenUtil().setWidth(570),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                  bottom: BorderSide(width: 1, color: Colors.black12)
+              )
+          ),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: childCategory.childCategoryList.length,
+            itemBuilder: (context, index){
+              return _rightInkWell(childCategory.childCategoryList[index]);
+            },
+          ),
+        );
+      },
+    );
+  }
+  
+  Widget _rightInkWell(BxMallSubDto item){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+        child: Text(
+          item.mallSubName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
+  }
+}
+
+
 
