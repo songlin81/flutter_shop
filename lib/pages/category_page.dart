d0dfec091dc972f7595bb12e19990826db993760
Lin Song <song.lin@volvo.com>
2020-06-19 10:09:23 +0800

    subCategory highlight done

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -162,7 +162,7 @@ class _RightCategoryNavState extends State<RightCategoryNav>{
             scrollDirection: Axis.horizontal,
             itemCount: childCategory.childCategoryList.length,
             itemBuilder: (context, index){
-              return _rightInkWell(childCategory.childCategoryList[index]);
+              return _rightInkWell(index, childCategory.childCategoryList[index]);
             },
           ),
         );
@@ -170,14 +170,22 @@ class _RightCategoryNavState extends State<RightCategoryNav>{
     );
   }
   
-  Widget _rightInkWell(BxMallSubDto item){
+  Widget _rightInkWell(int index, BxMallSubDto item){
+    bool isClick = false;
+    isClick = (index==Provide.value<ChildCategory>(context).childIndex)?true:false;
+
     return InkWell(
-      onTap: (){},
+      onTap: (){
+        Provide.value<ChildCategory>(context).changeChildIndex(index);
+      },
       child: Container(
         padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
         child: Text(
           item.mallSubName,
-          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(28),
+            color: isClick?Colors.pink:Colors.black
+          ),
         ),
       ),
     );
--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -3,9 +3,12 @@ import '../model/category.dart';
 
 class ChildCategory with ChangeNotifier{
   List<BxMallSubDto> childCategoryList = [];
-
+  int childIndex = 0;
 
   getChildCategory(List<BxMallSubDto> list){
+
+    childIndex = 0;
+
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId='00';
     all.mallCategoryId='00';
@@ -15,6 +18,11 @@ class ChildCategory with ChangeNotifier{
     childCategoryList=[all];
     childCategoryList.addAll(list);
     //childCategoryList = list;
+    notifyListeners();
+  }
+
+  changeChildIndex(index){
+    childIndex=index;
     notifyListeners();
   }
 }
