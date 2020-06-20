37064e1d80d4a511ce654d664bfdfebf770eccfd
Lin Song <song.lin@volvo.com>
2020-06-20 23:04:53 +0800

    prefs added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:fluro/fluro.dart';
 import './routers/routers.dart';
 import './routers/application.dart';
 import './provide/details_info.dart';
+import './provide/cart.dart';
 
 void main() {
   var counter = Counter();
@@ -15,12 +16,14 @@ void main() {
   var childCategory = ChildCategory();
   var categoryGoodsListProvide = CategoryGoodsListProvide();
   var detailsInfoProvide = DetailsInfoProvide();
+  var cartProvide = CartProvide();
 
   providers
     ..provide(Provider<Counter>.value(counter))
     ..provide(Provider<ChildCategory>.value(childCategory))
     ..provide(Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))
-    ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide));
+    ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))
+    ..provide(Provider<CartProvide>.value(cartProvide));
   runApp(ProviderNode(child: MyApp(), providers: providers));
 }
 
---
+++ lib/provide/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class CartProvide with ChangeNotifier {
+  String cartString="[]";
+
+  save(goodsId, goodsName, count, price, images) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    cartString=prefs.getString('cartInfo');
+    var temp=cartString==null?[]:json.decode(cartString.toString());
+    List<Map> tempList= (temp as List).cast();
+
+    bool isHave= false;
+    int ival=0;
+    tempList.forEach((item) {
+      if(item['goodsId']==goodsId){
+        tempList[ival]['count']=item['count']+1;
+        isHave=true;
+      }
+      ival++;
+    });
+
+    if(!isHave){
+      Map<String, dynamic> newGoods={
+        'goodsId':goodsId,
+        'goodsName':goodsName,
+        'count':count,
+        'price':price,
+        'images':images
+      };
+      tempList.add(newGoods);
+    }
+
+    cartString= json.encode(tempList).toString();
+    print(cartString);
+    prefs.setString('cartInfo', cartString);
+    notifyListeners();
+  }
+
+  remove() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('cartInfo');
+    print('All clear!');
+    notifyListeners();
+  }
+}
