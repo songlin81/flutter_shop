c5b18b39e53d99e534102a309302fc51eeaf6ef2
Lin Song <song.lin@volvo.com>
2020-06-22 11:44:25 +0800

    membership page completed

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -11,7 +11,9 @@ class MemberPage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           _topHeader(),
-
+          _orderTitle(),
+          _orderType(),
+          _actionList()
         ],
       ),
     );
@@ -46,12 +48,103 @@ class MemberPage extends StatelessWidget {
     );
   }
 
-  Widget _(){
+  Widget _orderTitle(){
     return Container(
-
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.black12)
+        )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
     );
   }
 
+  Widget _orderType(){
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.party_mode, size: 30),
+                Text('待付款')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.query_builder, size: 30),
+                Text('待发货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.directions_car, size: 30),
+                Text('待收货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.content_paste, size: 30),
+                Text('待评价')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _myListTile(String title) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.black12)
+        )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.blur_circular),
+        title: Text(title),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  Widget _actionList(){
+   return Container(
+     margin: EdgeInsets.only(top: 10),
+     child: Column(
+       children: <Widget>[
+         _myListTile('领取优惠券'),
+         _myListTile('已领取优惠券'),
+         _myListTile('地址管理'),
+         _myListTile('客服电话'),
+         _myListTile('关于我们')
+       ],
+     ),
+   );
+  }
 }
 
 //import 'package:provide/provide.dart';
