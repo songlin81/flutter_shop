a6efd577312c61b666955d8270212047b62510d1
Lin Song <song.lin@volvo.com>
2020-06-22 10:51:31 +0800

    cart navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './routers/routers.dart';
 import './routers/application.dart';
 import './provide/details_info.dart';
 import './provide/cart.dart';
+import './provide/currentIndex.dart';
 
 void main() {
   var counter = Counter();
@@ -17,13 +18,15 @@ void main() {
   var categoryGoodsListProvide = CategoryGoodsListProvide();
   var detailsInfoProvide = DetailsInfoProvide();
   var cartProvide = CartProvide();
+  var currentIndexProvide = CurrentIndexProvide();
 
   providers
     ..provide(Provider<Counter>.value(counter))
     ..provide(Provider<ChildCategory>.value(childCategory))
     ..provide(Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))
     ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))
-    ..provide(Provider<CartProvide>.value(cartProvide));
+    ..provide(Provider<CartProvide>.value(cartProvide))
+    ..provide(Provider<CurrentIndexProvide>.value(currentIndexProvide));
   runApp(ProviderNode(child: MyApp(), providers: providers));
 }
 
--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -5,6 +5,8 @@ import 'home_page.dart';
 import 'category_page.dart';
 import 'cart_page.dart';
 import 'member_page.dart';
+import 'package:provide/provide.dart';
+import '../provide/currentIndex.dart';
 
 //class IndexPage extends StatelessWidget{
 //  @override
@@ -20,13 +22,9 @@ import 'member_page.dart';
 //  }
 //}
 
-class IndexPage extends StatefulWidget{
-  _IndexPageState createState() => _IndexPageState();
-}
+class IndexPage extends StatelessWidget{
 
-class _IndexPageState extends State<IndexPage>{
-
-  final List<BottomNavigationBarItem> bottomTabs = [
+    final List<BottomNavigationBarItem> bottomTabs = [
     BottomNavigationBarItem(
       icon: Icon(CupertinoIcons.home),
       title: Text('首页')
@@ -46,46 +44,107 @@ class _IndexPageState extends State<IndexPage>{
   ];
 
   final List<Widget> tabBodies = [
-  //final List tabBodies = [
     HomePage(),
     CategoryPage(),
     CartPage(),
     MemberPage()
   ];
 
-  int currentIndex = 0;
-  var currentPage;
-
-  @override
-  void initState(){
-    currentPage = tabBodies[currentIndex];
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-
     ScreenUtil.init(context, width: 750, height: 1334);
-    print('${ScreenUtil.pixelRatio} - ${ScreenUtil.screenHeight} - ${ScreenUtil.screenWidth}');
-
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 245, 245, 1),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        items: bottomTabs,
-        onTap: (index){
-          setState(() {
-            currentIndex = index;
-            currentPage = tabBodies[currentIndex];
-          });
-        },
-      ),
-      body: IndexedStack(
-        index: currentIndex,
-        children: tabBodies,
-      )
-      //body: currentPage,
+    return Provide<CurrentIndexProvide>(
+      builder: (context, child, val) {
+        int currentIndex = Provide
+            .value<CurrentIndexProvide>(context)
+            .currentIndex;
+        return Scaffold(
+            backgroundColor: Color.fromRGBO(244, 245, 245, 1),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: currentIndex,
+              items: bottomTabs,
+              onTap: (index) {
+                Provide.value<CurrentIndexProvide>(context).changeIndex(index);
+              },
+            ),
+            body: IndexedStack(
+              index: currentIndex,
+              children: tabBodies,
+            )
+          //body: currentPage,
+        );
+      },
     );
   }
 }
+
+//class IndexPage extends StatefulWidget{
+//  _IndexPageState createState() => _IndexPageState();
+//}
+//
+//class _IndexPageState extends State<IndexPage>{
+//
+//  final List<BottomNavigationBarItem> bottomTabs = [
+//    BottomNavigationBarItem(
+//      icon: Icon(CupertinoIcons.home),
+//      title: Text('首页')
+//    ),
+//    BottomNavigationBarItem(
+//        icon: Icon(CupertinoIcons.search),
+//        title: Text('分类')
+//    ),
+//    BottomNavigationBarItem(
+//        icon: Icon(CupertinoIcons.shopping_cart),
+//        title: Text('购物车')
+//    ),
+//    BottomNavigationBarItem(
+//        icon: Icon(CupertinoIcons.profile_circled),
+//        title: Text('会员中心')
+//    )
+//  ];
+//
+//  final List<Widget> tabBodies = [
+//  //final List tabBodies = [
+//    HomePage(),
+//    CategoryPage(),
+//    CartPage(),
+//    MemberPage()
+//  ];
+//
+//  int currentIndex = 0;
+//  var currentPage;
+//
+//  @override
+//  void initState(){
+//    currentPage = tabBodies[currentIndex];
+//    super.initState();
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//    ScreenUtil.init(context, width: 750, height: 1334);
+//    print('${ScreenUtil.pixelRatio} - ${ScreenUtil.screenHeight} - ${ScreenUtil.screenWidth}');
+//
+//    return Scaffold(
+//      backgroundColor: Color.fromRGBO(244, 245, 245, 1),
+//      bottomNavigationBar: BottomNavigationBar(
+//        type: BottomNavigationBarType.fixed,
+//        currentIndex: currentIndex,
+//        items: bottomTabs,
+//        onTap: (index){
+//          setState(() {
+//            currentIndex = index;
+//            currentPage = tabBodies[currentIndex];
+//          });
+//        },
+//      ),
+//      body: IndexedStack(
+//        index: currentIndex,
+//        children: tabBodies,
+//      )
+//      //body: currentPage,
+//    );
+//  }
+//}
