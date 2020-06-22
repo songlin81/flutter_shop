b722293a2faa5a9f50e4b2a1681197c9ef25da14
Lin Song <song.lin@volvo.com>
2020-06-22 11:18:22 +0800

    member page header added

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MemberPage extends StatelessWidget{
+class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Provide<Counter>(
-            builder: (context, child, counter){
-              return Text(
-                '${counter.value}',
-                style: Theme.of(context).textTheme.headline4,
-              );
-            },
-          ),
-        )
+      appBar: AppBar(
+        title: Text("会员中心"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _topHeader(),
+
+        ],
+      ),
     );
   }
+
+  Widget _topHeader(){
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ClipOval(
+              child: Image.network('http://tvax3.sinaimg.cn/crop.37.0.958.958.180/68754f53ly8fq51134oj3j20sp0sp0y3.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              'Lin, Song',
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(36),
+                color: Colors.black54
+              ),
+            ),
+
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _(){
+    return Container(
+
+    );
+  }
+
 }
+
+//import 'package:provide/provide.dart';
+//import '../provide/counter.dart';
+//
+//class MemberPage extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        body: Center(
+//          child: Provide<Counter>(
+//            builder: (context, child, counter){
+//              return Text(
+//                '${counter.value}',
+//                style: Theme.of(context).textTheme.headline4,
+//              );
+//            },
+//          ),
+//        )
+//    );
+//  }
+//}
