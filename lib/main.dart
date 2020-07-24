dd669486ecbdcd04283b46c1312dfcfff321a4bc
Lin, Song <koalainuwo@hotmail.com>
2020-07-24 15:30:01 +0800

    i18n clean up

--- lib/main.dart
+++ lib/main.dart
@@ -51,25 +51,18 @@ class MyApp extends StatelessWidget{
         ),
         home: IndexPage(),
         localizationsDelegates: [
-          // THIS CLASS WILL BE ADDED LATER
-          // A class which loads the translations from JSON files
           AppLocalizations.delegate,
-          // Built-in localization of basic text for Material widgets
           GlobalMaterialLocalizations.delegate,
-          // Built-in localization for text direction LTR/RTL
           GlobalWidgetsLocalizations.delegate,
         ],
-        // Returns a locale which will be used by the app
         localeResolutionCallback: (locale, supportedLocales) {
-          // Check if the current device locale is supported
           for (var supportedLocale in supportedLocales) {
             if (supportedLocale.languageCode == locale.languageCode &&
                 supportedLocale.countryCode == locale.countryCode) {
               return supportedLocale;
             }
           }
-          // If the locale of the device is not supported, use the first one
-          // from the list (English, in this case).
+          // If the locale of the device is not supported, use the first one from the list
           return supportedLocales.first;
         },
         supportedLocales: [
